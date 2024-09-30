bda26ccc3ab6aed1ba4cc8656f65dc242ff56f6e
Nick Patrikeos <npatrikeos@atlassian.com>
2024-09-30 19:10:14 +1000

    Initial

---
+++ src/frontend/index.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import ForgeReconciler, { Text } from '@forge/react';
+import { invoke } from '@forge/bridge';
+
+const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
+  }, []);
+
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <Text>{data ? data : 'Loading...'}</Text>
+    </>
+  );
+};
+
+ForgeReconciler.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
